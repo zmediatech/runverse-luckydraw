854a1ccf794782919be8c6ffda57eb7e48e42619
Usman Mughal <125974637+usmanmughal96@users.noreply.github.com>
2025-07-10 20:41:40 +0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,18 @@ export default defineConfig({
         target: 'https://runverse-backend.vercel.app',
         changeOrigin: true,
         secure: false,
+        timeout: 5000,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('Proxy error:', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
       }
     }
   },
